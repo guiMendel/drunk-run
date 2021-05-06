60636c64d752b9c7c8bff940dfb4066aa8647a8d
LionelLR <45712054+LionelLR@users.noreply.github.com>
2021-05-06 15:11:03 +0200

    Update sdl.cpp
    -file organization
    -change of color
    -add collisionCheck();
    -add gameOver();
    -add meterCount()

--- src/sdl.cpp
+++ src/sdl.cpp
@@ -1,8 +1,52 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include "../include/sdl.hpp"
 #include <iostream>
 
 #define GROUND_Y height - (screenHeight / 2)
+
+/******************************************************************************
+ * Public methods
+******************************************************************************/
+
+void SDLWrapper::openWindow() {
+  // Aux local pointers
+  SDL_Window* windowPtr;
+  SDL_Renderer* rendererPtr;
+
+  // Create window & renderer
+  ensure(
+    SDL_CreateWindowAndRenderer(
+      // Window width & height
+      screenWidth,
+      screenHeight,
+      // Window flags
+      SDL_WINDOW_SHOWN,
+      // Pointer refs
+      &windowPtr,
+      &rendererPtr
+    ) == 0,
+    "Failed to create window and renderer"
+  );
+
+  // Store pointers
+  window.reset(windowPtr);
+  renderer.reset(rendererPtr);
+
+  //Initialize renderer color
+  SDL_SetRenderDrawColor(renderer.get(), 12, 20, 69, 255); //color of the night
+}
+
+void SDLWrapper::resolveEvents(void(*eventHandler)(void*, SDL_Event&),
+                                void* context) {
+  // Input event receiver
+  static SDL_Event event;
+
+  // Handle events on queue
+  while (SDL_PollEvent(&event) != 0) {
+    eventHandler(context, event);
+  }
+}
 
 void SDLWrapper::setCamera(int x, float z) {
   cameraX = x;
@@ -10,40 +54,85 @@ void SDLWrapper::setCamera(int x, float z) {
 
   // Destroy all objects that were left behind
   while (obstacles.size() > 0 && obstacles.front()->z < cameraZ) {
-    // Checks for collision
-    // collisionCheck();
+    // Checks for collision, TODO if collision : need to stop the game
+    collisionCheck();
 
     // Destroys this obstacle
     obstacles.pop_front();
   }
 }
 
-void SDLWrapper::drawFloor() {
-  // Get renderer
+void SDLWrapper::renderFrame(int score) {
+  // Get renderer pointer
   auto rendererPtr = renderer.get();
 
-  SDL_SetRenderDrawColor(rendererPtr, OBSTACLE_COLOR);
+  // Clear screen
+  SDL_SetRenderDrawColor(rendererPtr, 12, 20, 69, 255);
+  SDL_RenderClear(rendererPtr);
 
-  // Draw left border
-  SDL_RenderDrawLine(
-    rendererPtr,
-    x(-floorWidth / 2 - cameraX),
-    y(-screenHeight / 2 - CAMERA_HEIGHT),
-    x(0),
-    y(0)
-  );
+  if (collision) {
+    gameOver();
+  }else {
+    //Display the current score
+    meterCount(score);
 
-  // Draw right border
-  SDL_RenderDrawLine(
-    rendererPtr,
-    x(floorWidth / 2 - cameraX),
-    y(-screenHeight / 2 - CAMERA_HEIGHT),
-    x(0),
-    y(0)
-  );
+    // Draw floor
+    drawFloor();
+
+    // Draw each one of the obstacles
+    for (auto obstacle = obstacles.rbegin();
+              obstacle != obstacles.rend(); obstacle++) {
+      // Dereferentiate iterator and unique_ptr
+      drawObstacle(**obstacle);
+    }
+  }
+  // Update screen
+  SDL_RenderPresent(rendererPtr);
+
+  // std::cout << obstacles.size() << std::endl;
 }
 
-SDL_Rect SDLWrapper::makeRect(int bottomLeft, int width, int height, int distance) {
+void SDLWrapper::meterCount(int meter) {
+  auto rendererPtr = renderer.get();
+
+  std::string s = std::to_string(meter) + " m";
+  char const *text = s.c_str();
+  Text score(text, screenWidth/2 - 50, screenHeight/40, 70, 100);
+
+  Font font;
+  font.RenderText(rendererPtr, score);
+}
+
+/******************************************************************************
+ * Private methods
+******************************************************************************/
+
+void SDLWrapper::collisionCheck() {
+  int x1 = obstacles.front().get()->bottomLeft;
+  int x2 = x1 + obstacles.front().get()->width;
+
+  //The width of the player is screenWidth/2
+  int playerX1 = cameraX - screenWidth/4;
+  int playerX2 = cameraX + screenWidth/4;
+
+  if (!(x2 < playerX1) && !(x1 > playerX2)) {
+    collision = true;
+  }
+}
+
+void SDLWrapper::gameOver() {
+  auto rendererPtr = renderer.get();
+
+  Text text("Game Over", screenWidth/2 - 250, screenHeight/2 - 100, 200, 500);
+
+  SDL_Color yellow = SDL_Color{247, 216, 39, 255};
+
+  Font font("Fonts/game_over.ttf", 128, yellow);
+  font.RenderText(rendererPtr, text);
+}
+
+SDL_Rect SDLWrapper::makeRect(int bottomLeft, int width, 
+                                int height, int distance) {
   return SDL_Rect({
     x(perspective(bottomLeft, distance, AXIS_X)),
     y(perspective(GROUND_Y, distance, AXIS_Y)),
@@ -52,7 +141,41 @@ SDL_Rect SDLWrapper::makeRect(int bottomLeft, int width, int height, int distanc
     });
 }
 
-void SDLWrapper::drawSides(int bottomLeft, int width, int height, int distance) {
+void SDLWrapper::drawObstacle(Obstacle& obstacle) {
+  // Relative distance to camera
+  int distance = obstacle.z - cameraZ;
+
+  // Ignore obstacles beyond the camera (there shouldnt be any though)
+  if (distance < 0) return;
+
+  // Get renderer
+  auto rendererPtr = renderer.get();
+
+  // Draw sides
+  drawSides(obstacle.bottomLeft, obstacle.width, obstacle.height, distance);
+
+  // Paint color
+  SDL_SetRenderDrawColor(rendererPtr, OBSTACLE_COLOR);
+
+  // Draw front face
+  SDL_Rect outlineRect = makeRect(obstacle.bottomLeft, obstacle.width,
+                                   obstacle.height, distance);
+
+  // Filled
+  if (!wireframesOnly) SDL_RenderFillRect(rendererPtr, &outlineRect);
+  // Wireframe
+  else {
+    SDL_RenderDrawRect(rendererPtr, &outlineRect);
+
+    // Draw back face
+    outlineRect = makeRect(obstacle.bottomLeft, obstacle.width, obstacle.height,
+                                   distance + OBSTACLE_DEPTH);
+    SDL_RenderDrawRect(rendererPtr, &outlineRect);
+  }
+}
+
+void SDLWrapper::drawSides(int bottomLeft, int width,
+                              int height, int distance) {
   const int startingY = -(screenHeight / 2) + 5;
   const int finalY = -(screenHeight / 2) + height;
   const int startingX = bottomLeft;
@@ -95,96 +218,27 @@ void SDLWrapper::drawEdge(int pointX, int pointY, int distance) {
   );
 }
 
-void SDLWrapper::drawObstacle(Obstacle& obstacle) {
-  // Relative distance to camera
-  int distance = obstacle.z - cameraZ;
-
-  // Ignore obstacles beyond the camera (there shouldnt be any though)
-  if (distance < 0) return;
-
+void SDLWrapper::drawFloor() {
   // Get renderer
   auto rendererPtr = renderer.get();
 
-  // Draw sides
-  drawSides(obstacle.bottomLeft, obstacle.width, obstacle.height, distance);
-
-  // Paint color
   SDL_SetRenderDrawColor(rendererPtr, OBSTACLE_COLOR);
 
-  // Draw front face
-  SDL_Rect outlineRect = makeRect(obstacle.bottomLeft, obstacle.width, obstacle.height, distance);
-
-  // Filled
-  if (!wireframesOnly) SDL_RenderFillRect(rendererPtr, &outlineRect);
-  // Wireframe
-  else {
-    SDL_RenderDrawRect(rendererPtr, &outlineRect);
-
-    // Draw back face
-    outlineRect = makeRect(obstacle.bottomLeft, obstacle.width, obstacle.height, distance + OBSTACLE_DEPTH);
-    SDL_RenderDrawRect(rendererPtr, &outlineRect);
-  }
-}
-
-void SDLWrapper::renderFrame() {
-  // Get renderer pointer
-  auto rendererPtr = renderer.get();
-
-  // Clear screen
-  SDL_SetRenderDrawColor(rendererPtr, 0xFF, 0xFF, 0xFF, 0xFF);
-  SDL_RenderClear(rendererPtr);
-
-  // Draw floor
-  drawFloor();
-
-  // Draw each one of the obstacles
-
-  for (auto obstacle = obstacles.rbegin(); obstacle != obstacles.rend(); obstacle++) {
-    // Dereferentiate iterator and unique_ptr
-    drawObstacle(**obstacle);
-  }
-
-  // Update screen
-  SDL_RenderPresent(rendererPtr);
-
-  // std::cout << obstacles.size() << std::endl;
-}
-
-
-void SDLWrapper::resolveEvents(void(*eventHandler)(void*, SDL_Event&), void* context) {
-  // Input event receiver
-  static SDL_Event event;
-
-  // Handle events on queue
-  while (SDL_PollEvent(&event) != 0) {
-    eventHandler(context, event);
-  }
-}
-
-void SDLWrapper::openWindow() {
-  // Aux local pointers
-  SDL_Window* windowPtr;
-  SDL_Renderer* rendererPtr;
-
-  // Create window & renderer
-  ensure(
-    SDL_CreateWindowAndRenderer(
-      // Window width & height
-      screenWidth,
-      screenHeight,
-      // Window flags
-      SDL_WINDOW_SHOWN,
-      // Pointer refs
-      &windowPtr,
-      &rendererPtr
-    ) == 0,
-    "Failed to create window and renderer"
+  // Draw left border
+  SDL_RenderDrawLine(
+    rendererPtr,
+    x(-floorWidth / 2 - cameraX),
+    y(-screenHeight / 2 - CAMERA_HEIGHT),
+    x(0),
+    y(0)
   );
 
-  // Store pointers
-  window.reset(windowPtr);
-  renderer.reset(rendererPtr);
-
-  //Initialize renderer color
-  SDL_SetRenderDrawColor(renderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+  // Draw right border
+  SDL_RenderDrawLine(
+    rendererPtr,
+    x(floorWidth / 2 - cameraX),
+    y(-screenHeight / 2 - CAMERA_HEIGHT),
+    x(0),
+    y(0)
+  );
 }
